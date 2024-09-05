1d1fe55d936213d332e40f83c1dadbf2d0713e57
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-09-05 18:59:08 -0300

    first commit

---
+++ src/main/java/com/project/demo_parking_api/DemoParkingApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo_parking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParkingApiApplication.class, args);
+	}
+
+}

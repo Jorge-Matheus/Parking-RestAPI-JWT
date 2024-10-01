a0d85b28d53e0048eca82c918c7b85963b781f1e
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-10-01 20:15:45 -0300

    Creating cliente intercept filter, JwtAuthorizationFilter

---
+++ src/main/java/com/project/demo_parking_api/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,51 @@
+package com.project.demo_parking_api.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtUserDetailsService detailsService;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		
+		final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
+		if(token == null || !token.startsWith(JwtUtils.JWT_BEARER)) {
+			System.out.println("JWT Token está nulo, vazio ou não iniciado com 'Bearer '. ");
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		if(!JwtUtils.isTokenValid(token)) {
+			System.out.println("JWT Token está invalido ou expirado.");
+			filterChain.doFilter(request, response);
+			return;
+		}
+		String username = JwtUtils.getUsernameFromToken(token);
+		toAuthentication(request, username);
+		filterChain.doFilter(request, response);
+	}
+
+	private void toAuthentication(HttpServletRequest request, String username) {
+		UserDetails userDetails = detailsService.loadUserByUsername(username);
+		UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userDetails, null, userDetails.getAuthorities());
+		authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+		SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+	}
+}

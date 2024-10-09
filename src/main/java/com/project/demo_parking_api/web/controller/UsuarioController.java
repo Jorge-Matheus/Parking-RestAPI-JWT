6c5145198c069119ce82bcfe8f39985703acbb7b
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-10-09 14:55:06 -0300

    Administrator access permission to the location feature by Id

--- src/main/java/com/project/demo_parking_api/web/controller/UsuarioController.java
+++ src/main/java/com/project/demo_parking_api/web/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -70,6 +71,7 @@ public class UsuarioController {
 			}
 			)
 	@GetMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
 		Usuario user = usuarioService.buscarPorId(id);
 		return ResponseEntity.ok(UsuarioMapper.toDto(user));
